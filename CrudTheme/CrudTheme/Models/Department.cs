317ddde4230bf33ef697f4312f7f8a7a1f968281
Iqra Khaliq <iqra44938@gmail.com>
2023-11-13 08:58:23 -0800

    connection/theme/categorymodel

---
+++ CrudTheme/CrudTheme/Models/Department.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CrudTheme.Models
+{
+    public class Department
+    {
+        [Key]
+        int DepartmentId { get; set; }
+        [Required]
+        string DepartmentName { get; set; }
+       
+
+
+    }
+}
