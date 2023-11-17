bbb34c3ebd5ade455231011b96e525cd87573804
Iqra Khaliq <iqra44938@gmail.com>
2023-11-17 08:35:09 -0800

    css

--- CrudTheme/CrudTheme/Models/Employee.cs
+++ CrudTheme/CrudTheme/Models/Employee.cs
@@ -2,5 +2,10 @@
 {
     public class Employee
     {
+        public int EmployeeId { get; set; }
+        public string EmployeeName { get; set; }
+        public string Shift { get; set; }
+
+
     }
 }
